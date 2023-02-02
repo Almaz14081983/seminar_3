6859326048bf430d3fe1e19223450a4c6edd9b74
Almaz <minnexanov.a@mail.ru>
2023-02-02 21:32:58 +0300

    tas 3

---
+++ task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Clear();
+Console.Write("Ввведите целое число: ");
+int unser  = int.Parse(Console.ReadLine());
+int counter = 1;
+while (counter<= unser)
+{
+    Console.Write(counter*counter+ ", ");
+    counter++;
+}
+Console.Write(counter*counter+ ", ");
