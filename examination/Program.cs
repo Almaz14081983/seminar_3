38a4414b444cee1ec82623d05476864bb29de1d2
Almaz <minnexanov.a@mail.ru>
2023-02-03 15:58:18 +0300

    examination

--- examination/Program.cs
+++ examination/Program.cs
@@ -1,3 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+
+Console.Clear();
+Console.WriteLine("Ввведите число: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+
+if (number < 100)
+{
+    Console.WriteLine("третий цифры нет: ");
+}
+else
+{
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    result = number %10;
+    Console.WriteLine(result);
+}
