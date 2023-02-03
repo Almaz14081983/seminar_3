bf2406b63d5c717ac3f76f6b806a3a25f69e3eda
Almaz <minnexanov.a@mail.ru>
2023-02-03 17:18:35 +0300

    task 2в

---
+++ task2B/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.Write("Введите номер четверти = ");
+int quarter = int.Parse(Console.ReadLine());
+
+if ((quarter >4) || (quarter <1)) Console.Write("нет четверти: ");
+
+if (quarter == 1) Console.Write("x > 0 , y > 0 ");
+if (quarter == 2) Console.Write("x < 0 , y > 0 ");
+if (quarter == 3) Console.Write("x < 0 , y < 0 ");
+if (quarter == 4) Console.Write("x > 0 , y < 0 ");
+
