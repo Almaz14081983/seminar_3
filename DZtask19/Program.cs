1e5123d06a966495fdd9c30d59a3253022bcd1c0
Almaz <minnexanov.a@mail.ru>
2023-02-04 16:28:33 +0300

    DZ19

--- DZtask19/Program.cs
+++ DZtask19/Program.cs
@@ -6,20 +6,29 @@
 // 12821 -> да
 
 Console.Clear();
-Console.WriteLine("Ввведите число: ");
+Console.WriteLine("Введите число: ");
 int num = int.Parse(Console.ReadLine()!);
 
 int num1 = num/10000;
-Console.WriteLine($"первая цифра {num1}");
+// Console.WriteLine($"первая цифра {num1}");
 
 int num2 = (num/1000)%10;
-Console.WriteLine($"вторая цифра {num2}");
+// Console.WriteLine($"вторая цифра {num2}");
 
 int num4 = (num%100)/10;
-Console.WriteLine($"четвертая цифра {num4}");
+// Console.WriteLine($"четвертая цифра {num4}");
 
 int num5 = num%10;
-Console.WriteLine($"пятая цифра {num5}");
+// Console.WriteLine($"пятая цифра {num5}");
+
+if ((num1 == num5) && (num2 == num4))
+{
+    Console.Write("число является палиндромом");
+}
+else
+{
+    Console.Write("число не является палиндромом");
+}
 
 
 
