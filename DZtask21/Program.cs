16a77a5e21f087f61e7b6ae9669b46dd76da3ff4
Almaz <minnexanov.a@mail.ru>
2023-02-04 09:25:58 +0300

    DZ21

---
+++ DZtask21/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход координаты 
+// двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+// AB = (Xb-Xa)(Xb-Xa) + (Yb-Ya)(Yb-Ya) + (Zb-Za)(Zb-Za)
+
+Console.Clear();
+Console.Write("Введите координаты точки А, Xa = ");
+double Xa = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки А, Ya = ");
+double Ya = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки A, Za = ");
+double Za = double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите координаты точки B, Xb = ");
+double Xb = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки B, Yb = ");
+double Yb = double.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки B, Zb = ");
+double Zb = double.Parse(Console.ReadLine()!);
+
+double sqrab = ((Xb - Xa)*(Xb - Xa) + (Yb - Ya)*(Yb - Ya) + (Zb - Za)*(Zb - Za));
+double result = Math.Sqrt(sqrab);
+double result2 = Math.Round(result,2);
+// double result2 = result%1000;
+
+Console.Write($"расстояние между ними {result2}");
