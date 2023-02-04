d63c36a62035b2056988dbc6fb6ff902c856e51c
Almaz <minnexanov.a@mail.ru>
2023-02-04 17:51:47 +0300

    DZ23

---
+++ DZtask23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23: Напишите программу, которая принимает на
+// вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27.
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число N = ");
+int N = int.Parse(Console.ReadLine()!);
+
+int num = 1;
+while (num < N)
+{
+    Console.Write(num*num*num + " , ");
+    num++;
+}
+Console.Write(num*num*num + ". ");
